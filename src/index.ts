0eb79e6aedcd195958b6c1e717bcc51e81561cc9
manelgarcia <manelet@gmail.com>
2020-04-13 08:27:04 +0200

    debug biuld on heroku

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import cors from 'cors'
 import { resolve } from 'path'
 import { config } from 'dotenv'
 
+console.log('ENVVVV', process.env.NODE_ENV);
+
 if (process.env.NODE_ENV !== 'production') {
   config({ path: resolve(__dirname, "./.env") })
 }
