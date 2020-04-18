62a83593b7565a848726fe1597c4d8db08921e15
manelgarcia <manelet@gmail.com>
2020-04-18 14:37:32 +0200

    hidden credentials for mongo

--- src/mongo.ts
+++ src/mongo.ts
@@ -1,11 +1,18 @@
 import mongoose from 'mongoose'
 
+const {
+  MONGO_USER,
+  MONGO_PW,
+  MONGO_URL,
+  MONGO_DB
+} = process.env
+
 const defaultOpts = {
   useUnifiedTopology: true, 
   useNewUrlParser: true
 }
 
-mongoose.connect('mongodb+srv://bocado:Mongo123_!@cluster0-iqnt7.mongodb.net/bocado', defaultOpts)
+mongoose.connect(`mongodb+srv://${MONGO_USER}:${MONGO_PW}@${MONGO_URL}/${MONGO_DB}`, defaultOpts)
 
 export const connect = async () =>
   new Promise((resolve, reject) => {
