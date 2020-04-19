ae360490309fb71429bd5b4701f1c2bbab13a266
manelgarcia <manelet@gmail.com>
2020-04-19 07:29:32 +0200

    chore: moved interface IResolver for resolver params to schema index

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,17 +1,36 @@
 import { SchemaComposer } from 'graphql-compose'
+// import { GraphQLUpload } from 'apollo-server-express'
+import { GraphQLUpload } from 'graphql-upload';
+
 import { IngredientQuery, IngredientMutation } from './Ingredient'
 import { UserQuery, UserMutation } from './User'
+import { FileQuery, FileMutation } from './File'
 
 const schemaComposer = new SchemaComposer()
 
 schemaComposer.Query.addFields({
   ...IngredientQuery,
-  ...UserQuery
+  ...UserQuery,
+  ...FileQuery
 })
 
 schemaComposer.Mutation.addFields({
   ...IngredientMutation,
-  ...UserMutation
+  ...UserMutation,
+  ...FileMutation
 })
 
+// schemaComposer.set('Upload', GraphQLUpload);
+
+schemaComposer.addTypeDefs(`
+  scalar Upload
+`);
+
 export default schemaComposer.buildSchema()
+
+export interface IResolver {
+  source: any,
+  args: any,
+  context: any,
+  info: any
+}
