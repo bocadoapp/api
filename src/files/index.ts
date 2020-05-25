a73b7e7bc82712b9fb68f1544ccf8e749ff7d482
manelgarcia <manelet@gmail.com>
2020-05-25 06:48:30 +0200

    chore: lint

--- src/files/index.ts
+++ src/files/index.ts
@@ -2,17 +2,17 @@ import AWSMock from 'mock-aws-s3'
 import path from 'path'
 
 AWSMock.config.basePath = path.join(__dirname, '../../tmp')
-const s3 = new AWSMock.S3({ params: { Bucket: 'bocado' }})
+const s3 = new AWSMock.S3({ params: { Bucket: 'bocado' } })
 
 export const upload = async (file: any) =>
-  new Promise ((res, rej) =>
+  new Promise((resolve, reject) =>
     s3.upload(
       { Bucket: 'bocado', Key: file.filename, Body: file.createReadStream() },
       (err: any, data: any) => {
         if (err) {
-          return rej(err)
+          return reject(err)
         }
-        return res(data)
+        return resolve(data)
       }
-    )  
+    )
   )
